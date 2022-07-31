7abea091c7ab9ce9a476a434ff87cc51d5425551
Kate <dvornichenko.e@gmail.com>
2022-08-01 01:30:44 +0300

    Complete task 5

--- main.js
+++ main.js
@@ -60,3 +60,22 @@ function checkPorchAndFloor(roomsOnFloor, floors, roomNumber) {
 }
 
 checkPorchAndFloor(3, 9, 456);
+
+//Task 5
+console.log("Task 5:");
+function piramid(iter) {
+
+  let item = iter * 2 - 1;
+  for (let i=0; i < iter; i++) {
+    let row = "";
+    for (let j = 1; j <= item; j++) {
+      if (j === iter || j >= (iter-i) && j <= (iter+i)) {
+        row += "#";
+      } else {
+        row += "-";
+      }
+    }
+    console.log(row);
+  }
+}
+piramid(6);
