c901e5833fe2bbcf74f25475a295425a74477758
Kate <dvornichenko.e@gmail.com>
2022-08-19 13:44:11 +0300

    hw7

--- main.js
+++ main.js
@@ -10,19 +10,3 @@ const eyesColor = condidateArr.map((el) => el.eyeColor);
 const unique = new Set(eyesColor);
 
 console.log(unique);
-
-function piramid(iter) {
-  let item = iter * 2 - 1;
-  for (let i = 0; i < iter; i++) {
-    let row = "";
-    for (let j = 1; j <= item; j++) {
-      if (j === iter || (j >= iter - i && j <= iter + i)) {
-        row += "#";
-      } else {
-        row += "-";
-      }
-    }
-    console.log(row);
-  }
-}
-piramid(6);
