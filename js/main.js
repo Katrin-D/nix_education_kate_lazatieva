79b1a2d8e7a6ff57ad5e16268246df9227eaa5fb
Kate <dvornichenko.e@gmail.com>
2022-08-24 20:51:37 +0300

    HW8

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const photo = document.getElementById("photo");
 const title = document.getElementById("title");
 const stockStatus = document.getElementById("stock_status");
 const price = document.getElementById("price");
+const reviews = document.getElementById("reviews");
+const orders = document.getElementById("orders");
 
 items.map((el) => {
   let newCard = document.createElement("div");
@@ -14,6 +16,8 @@ items.map((el) => {
   title.textContent = el.name;
   stockStatus.textContent = el.orderInfo.inStock;
   price.textContent = el.price;
+  reviews.textContent = el.orderInfo.reviews;
+  orders.textContent = Math.floor(Math.random() * 1000);
   newCard.innerHTML = card.innerHTML;
   cards_container.append(newCard);
 });
