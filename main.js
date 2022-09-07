29c30cda0f04904a3c4c37d42c7b962e49c93292
Kate <dvornichenko.e@gmail.com>
2022-09-07 16:22:15 +0300

    Added tasks from data

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 import { tasksData } from "./data.js"
 
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const d = new Date();
-let dateNow = document.querySelector(".dateNow");
+setDate();
+function setDate() {
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const d = new Date();
+    let dateNow = document.querySelector(".dateNow");
 
-dateNow.textContent = "Today: " + d.toLocaleDateString() + ", " + weekday[d.getUTCDay()];
+    dateNow.textContent = "Today: " + d.toLocaleDateString() + ", " + weekday[d.getUTCDay()];
+}
 
+const tasksContainer = document.querySelector(".tasks-container")
 tasksData.map((el) => {
-
+let task = document.createElement("div");
+    task.classList = "task";
+    task.innerHTML = `<p>${el.title}</p>`;
+    tasksContainer.appendChild(task);
 })
