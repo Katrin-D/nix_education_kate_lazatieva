4cd60fca793cdf3e39660a0aad0e7a9ed72d3cb6
Kate <dvornichenko.e@gmail.com>
2022-09-15 17:10:56 +0300

    Some fixes

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import { tasksData } from "./data.js"
 import "./style.scss"
 import variables from "./variables.scss";
-console.log(parseInt(variables.lineHeight));
 
 setDate();
 function setDate() {
@@ -13,12 +12,36 @@ function setDate() {
 }
 
 const tasksContainer = document.querySelector(".tasks-container")
-tasksData.map((el) => {
+const taskWithStyles = tasksData.reduce((previousValue, currentValue) => {
+    const newTask = {
+        ...currentValue,
+        left: 0,
+    }
+
+    const prevElement =
+        previousValue && previousValue[previousValue.length - 1];
+
+    if (
+        prevElement &&
+        currentValue.start > prevElement.start &&
+        currentValue.start < prevElement.start + prevElement.duration
+
+    ) { newTask.left = prevElement.left + 200}
+
+   return  previousValue.concat([newTask]);
+}, []);
+
+console.log(taskWithStyles)
+
+
+taskWithStyles.map((el) => {
     let heightOfTask = parseInt(variables.lineHeight) / 60 * el.duration;
     let task = document.createElement("div");
+
     task.classList = "task";
     task.style.height = `${heightOfTask}px`;
     task.style.top = `${el.start}px`;
+    task.style.left = `${el.left}px`;
     task.innerHTML = `<p>${el.title}</p>`;
     tasksContainer.appendChild(task);
 })
