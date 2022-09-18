1e16b690447ad7ed5af12df8a73fc9f757fef57a
Kate <dvornichenko.e@gmail.com>
2022-09-18 16:46:27 +0300

    Add modal

--- script/main.js
+++ script/main.js
@@ -1,75 +1,111 @@
-import { tasksData } from "../data/data.js"
-import "../style/style.scss"
-import variables from "../style/variables.scss";
+import { tasksData } from "../data/data.js";
+/*import "../style/style.scss";*/
+/*import variables from "../style/variables.scss";*/
 
 setDate();
 function setDate() {
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const d = new Date();
-    let dateNow = document.querySelector(".dateNow");
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const d = new Date();
+  let dateNow = document.querySelector(".dateNow");
 
-    dateNow.textContent = "Today: " + d.toLocaleDateString() + ", " + weekday[d.getUTCDay()];
+  dateNow.textContent =
+    "Today: " + d.toLocaleDateString() + ", " + weekday[d.getUTCDay()];
 }
 
-const createTask = (el) => {
-    let heightOfTask = parseInt(variables.lineHeight) / 60 * el.duration;
-    let task = document.createElement("div");
+const tasksContainer = document.querySelector(".tasks-container");
 
-    task.classList.add("task");
-    task.style.height = `${heightOfTask}px`;
-    task.style.top = `${el.start}px`;
-    task.style.left = `${el.left}px`;
-    task.innerHTML = `<p>${el.title}</p>`;
-    tasksContainer.appendChild(task);
-}
-
-
-const tasksContainer = document.querySelector(".tasks-container")
-const taskWithStyles = tasksData.reduce((previousValue, currentValue) => {
-    const newTask = {
+const tasksWithStyles = () => {
+  return tasksData
+    .sort((el1, el2) => el1.start - el2.start)
+    .reduce((previousValue, currentValue) => {
+      const newTask = {
         ...currentValue,
         left: 0,
-    }
+        width: "100%",
+      };
 
-    const prevElement =
+      const prevElement =
         previousValue && previousValue[previousValue.length - 1];
 
-    if (
+      if (
         prevElement &&
         currentValue.start > prevElement.start &&
         currentValue.start < prevElement.start + prevElement.duration
+      ) {
+        newTask.left = prevElement.left + 200;
+        newTask.width = `calc(100% - ${prevElement.left + 200}px)`;
+        prevElement.width = "200px";
+      }
 
-    ) { newTask.left = prevElement.left + 200}
+      return previousValue.concat([newTask]);
+    }, []);
+};
 
-   return  previousValue.concat([newTask]);
-}, []);
+const createTask = (el) => {
+  let height = 60; /*let heightOfTask = (parseInt(variables.lineHeight) / 60) * el.duration;*/
+  let heightOfTask = (height / 60) * el.duration;
+  let task = document.createElement("div");
 
-console.log(taskWithStyles)
+  task.classList.add("task");
+  task.style.height = `${heightOfTask}px`;
+  task.style.width = el.width;
+  task.style.top = `${el.start}px`;
+  task.style.left = `${el.left}px`;
+  task.innerHTML = `<p>${el.title}</p>`;
+
+  tasksContainer.appendChild(task);
+};
+
+tasksWithStyles().map((el) => {
+  createTask(el);
+});
+
+console.log(tasksWithStyles());
+
+const modal = document.querySelector(".modal-container");
+tasksContainer.addEventListener("click", (event) => {
+  event.stopPropagation();
+  let containerTopPosition = tasksContainer.getBoundingClientRect().top;
+  let start = event.pageY - containerTopPosition;
+  const timeStart = document.getElementById("time-start");
+  const timeEnd = document.getElementById("time-end");
+
+  console.log(start)
+  let newTask = {
+    start: start,
+    duration: 15,
+    title: "New Task",
+  };
 
 
-taskWithStyles.map((el) => {
+  timeStart.value = Math.floor((start)/ 60)+8;
+  timeEnd.value = Math.floor((start)/ 60)+8;
+
+
+  tasksData.push(newTask);
+  modal.classList.toggle("hidden");
+  console.log(tasksWithStyles());
+
+  /*  tasksWithStyles().map((el) => {
     createTask(el);
-})
+  });*/ //все отрисовіваются еще раз, поверх старых
 
+  createTask(newTask); //не учитывает все предыдущие таски
+});
 
-const tasksSpace = document.querySelector(".task-space")
-const modal = document.querySelector(".modal-container")
-tasksSpace.addEventListener("click", ($event) => {
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) modal.classList.toggle("hidden");
+});
 
-    /*let newTask = {
-        start: 0,
-        duration: 15,
-        title: "New Task"};*/
-
-    /*modal.classList.toggle("hidden")*/
-    console.log($event)
-})
-/*modal.addEventListener("click", (e) => {
-    if(e.target === modal)
-    modal.classList.toggle("hidden");
-})*/
-
-
-
-
-
+const colorPicker = document.getElementById("newTask-color");
+colorPicker.addEventListener("change", (event) => {
+  /*variables.taskColor = event.target.value*/
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,62 @@
 const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    main: path.resolve(__dirname, "./script/main.js"),
-  },
-  output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      // SCSS ALL EXCEPT MODULES
-      {
-        test: /\.scss$/i,
-        exclude: /\.module\.scss$/i,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              modules: {
-                mode: "icss",
-              },
+    mode: "development",
+    /*  entry: {
+        main: './script/main.js',
+        /!*main: path.resolve(__dirname, "./script/main.js"),*!/
+      },*/
+    entry: './script/main.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        /*rules: [
+            // SCSS ALL EXCEPT MODULES
+            {
+                test: /\.scss$/i,
+                exclude: /\.module\.scss$/i,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                mode: "icss",
+                            },
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ],
             },
-          },
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
-      // --------
-      // SCSS MODULES
-      {
-        test: /\.module\.scss$/i,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              modules: {
-                mode: "local",
-              },
+            // --------
+            // SCSS MODULES
+            {
+                test: /\.module\.scss$/i,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                mode: "local",
+                            },
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ],
             },
-          },
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
-    ],
-  },
+        ],*/
+    },
 };
