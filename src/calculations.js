627c362edda09399cbe5970514abc78987b2bc55
Kate <dvornichenko.e@gmail.com>
2022-10-25 15:52:15 +0300

    main NodeJS branch

--- src/calculations.js
+++
@@ -1,8 +0,0 @@
-const { square, cube } = require("./index.js");
-
-const squarePlusCube = (num) => {
-  return square(num) + cube(num);
-};
-
-//to check do "npm run startCalc"
-console.log(squarePlusCube(3));
